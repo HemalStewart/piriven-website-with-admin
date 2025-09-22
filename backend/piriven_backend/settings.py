b8af9f25846e1e10d4cb2eb34ddbfec89fcc9375
HemalStewart <nuwanhemal@gmail.com>
2025-09-22 12:31:21 +0530

    update server settings.py

--- backend/piriven_backend/settings.py
+++ backend/piriven_backend/settings.py
@@ -126,6 +126,7 @@ REST_FRAMEWORK = {
 # =========================
 # For dev: Next.js frontend running locally
 CORS_ALLOWED_ORIGINS = [
+    "https://piriven.moe.gov.lk",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://localhost:3000",
@@ -140,6 +141,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # CSRF trusted origins
 CSRF_TRUSTED_ORIGINS = [
+    "https://piriven.moe.gov.lk",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
 ]
